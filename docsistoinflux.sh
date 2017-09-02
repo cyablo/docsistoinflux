78776d5055f171e57dc56ec1eed19f7e14b6c293
Daniel Wandrei <cyablo@cyablo.de>
2017-09-02 23:36:17 +0200

    Workaround for Astrometa DVB-T/T2/C USB-Stick
    Added workaround to make dvbtune work with Astrometa DVB-T/T2/C USB-Tuner. Script will check at start if DVB adapter0 is Astrometa and do some magic then.

--- docsistoinflux.sh
+++ docsistoinflux.sh
@@ -2,13 +2,23 @@
 
 echo 0 > /sys/module/dvb_core/parameters/dvb_powerdown_on_sleep
 
-freq64="698000000 706000000 714000000 722000000 762000000 770000000 778000000 786000000 794000000 802000000 810000000 818000000"
-freq256="546000000 554000000 562000000 570000000 578000000 586000000 594000000 602000000 666000000 674000000 682000000 690000000"
+# Workaround for Astrometa DVB-T/T2/C USB-Stick to change frontends and make it work with dvbtune
+frontend0realtek=`dvbsnoop -frontend /dev/dvb/adapter0/frontend0 -s feinfo | grep Realtek | wc -l`
+frontend1panasonic=`dvbsnoop -frontend /dev/dvb/adapter0/frontend1 -s feinfo | grep Panasonic | wc -l`
+if [ $frontend0realtek == "1" ] && [ $frontend1panasonic == "1" ]; then
+ mv /dev/dvb/adapter0/frontend0 /dev/dvb/adapter0/frontend2
+ mv /dev/dvb/adapter0/frontend1 /dev/dvb/adapter0/frontend0
+ mv /dev/dvb/adapter0/frontend2 /dev/dvb/adapter0/frontend1
+ dvb-fe-tool --adapter=0 --frontend=0 --set-delsys=DVB-C
+fi
 
-influxhost="influxdb.local.domain"
+freq256="546000000 554000000 562000000 570000000 578000000 586000000 594000000 602000000 666000000 674000000 682000000 690000000"
+freq64="698000000 706000000 714000000 722000000 762000000 770000000 778000000 786000000 794000000 802000000 810000000 818000000"
+
+influxhost="influxhost"
 influxuser="user"
-influxpass="passwd"
-influxdb="docsis"
+influxpass="pass"
+influxdb="db"
 
 for freq1 in $freq256
 do
