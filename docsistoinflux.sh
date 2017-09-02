be26a7a7ebb734cdabe12fad5e166a9046a6b5c4
Daniel Wandrei <cyablo@cyablo.de>
2017-09-02 23:38:02 +0200

    More spaces :)

--- docsistoinflux.sh
+++ docsistoinflux.sh
@@ -6,10 +6,10 @@ echo 0 > /sys/module/dvb_core/parameters/dvb_powerdown_on_sleep
 frontend0realtek=`dvbsnoop -frontend /dev/dvb/adapter0/frontend0 -s feinfo | grep Realtek | wc -l`
 frontend1panasonic=`dvbsnoop -frontend /dev/dvb/adapter0/frontend1 -s feinfo | grep Panasonic | wc -l`
 if [ $frontend0realtek == "1" ] && [ $frontend1panasonic == "1" ]; then
- mv /dev/dvb/adapter0/frontend0 /dev/dvb/adapter0/frontend2
- mv /dev/dvb/adapter0/frontend1 /dev/dvb/adapter0/frontend0
- mv /dev/dvb/adapter0/frontend2 /dev/dvb/adapter0/frontend1
- dvb-fe-tool --adapter=0 --frontend=0 --set-delsys=DVB-C
+    mv /dev/dvb/adapter0/frontend0 /dev/dvb/adapter0/frontend2
+    mv /dev/dvb/adapter0/frontend1 /dev/dvb/adapter0/frontend0
+    mv /dev/dvb/adapter0/frontend2 /dev/dvb/adapter0/frontend1
+    dvb-fe-tool --adapter=0 --frontend=0 --set-delsys=DVB-C
 fi
 
 freq256="546000000 554000000 562000000 570000000 578000000 586000000 594000000 602000000 666000000 674000000 682000000 690000000"
